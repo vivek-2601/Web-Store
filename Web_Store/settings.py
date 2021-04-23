941583352b87d74adc355b9ab47153f436a1667c
ajay-makvana <ajaymakvana2001@gmail.com>
2021-04-23 09:00:40 +0530

    Base Template added and according to that all other templates changed

--- Web_Store/settings.py
+++ Web_Store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'Web_Store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'Templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
